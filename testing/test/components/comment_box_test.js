e361439365c53c36347f92ab376d629dc0bc4931
Leo Lee <nxl1969@gmail.com>
2017-03-26 18:33:56 -0400

    Updated the comment box component and its testing file

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -20,4 +20,19 @@ describe('CommentBox',()=>{
 		
 		expect(component.find('button')).to.exist;
 	});
+	
+	describe('entering some text',()=>{
+		beforeEach(()=>{
+			component.find('textarea').simulate('change','new comment');
+		});
+		it('show that text in the textarea',()=>{
+			expect(component.find('textarea')).to.have.value('new comment');
+		});
+
+		it('when submitted, clear the input',()=>{
+			component.simulate('submit');
+			expect(component.find('textarea')).to.have.value('');
+		});
+	});
+	
 });
