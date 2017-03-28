ea18e2baaf2f12aa2ba7e79e946aaeb8dbaa280c
Leo Lee <nxl1969@gmail.com>
2017-03-28 00:31:52 -0400

    Add comment list test file

---
+++ testing/test/components/comment_list_test.js
@@ -0,0 +1,19 @@
+import {renderComponent, expect} from '../test_helper';
+import CommentList from '../../src/components/comment_list';
+
+describe('CommentList',()=>{
+	let component;
+	beforeEach(()=>{
+		const props = {comments:['my comment','other comment']};
+		component = renderComponent(CommentList,null,props);
+	});
+	
+	it('shows an LI for each comment',()=>{
+		expect(component.find('li').length).to.equal(2);
+	});
+	
+	it('shows each comment that is provided',()=>{
+		expect(component).to.contain('my comment');
+		expect(component).to.contain('other comment');
+	});
+})
