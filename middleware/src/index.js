454baff39195ca2891645a4b796c69e102a92da8
Leo Lee <nxl1969@gmail.com>
2017-04-04 00:14:49 -0400

    Add the correct boilerplate files

--- middleware/src/index.js
+++ middleware/src/index.js
@@ -5,16 +5,11 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
-import {Router, Route, browserHistory} from 'react-router';
-import Resources from './components/resources';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory}>
-		<Route path='/' component={App}>
-			<Route path="resources" component={Resources}></Route>	
-		</Route>
-	</Router>
+    <App />
   </Provider>
   , document.querySelector('.container'));
