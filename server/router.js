d7a05c3fb40df27d5ed5f8ffb5546f47c0fd48d3
Leo Lee <nxl1969@gmail.com>
2017-04-12 01:01:36 -0400

    Add signin route. Return a token if email and password are verified

--- server/router.js
+++ server/router.js
@@ -1,4 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt',{session:false});//default is cookie based. since we use jwt token, we need to tell it to not use session
+const requireSignin = passport.authenticate('local',{session:false});
 module.exports = function(app){
+	app.get('/',requireAuth,function(req,res){
+		res.send({
+			hi:'there'
+		});
+	});
+	app.post('/signin',requireSignin,Authentication.signin);
 	app.post('/signup',Authentication.signup);
 }
