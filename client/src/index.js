461a3ed3eebc25e910275fa3f656d48a0c243da1
Leo Lee <nxl1969@gmail.com>
2017-04-14 00:35:03 -0400

    Add signin route

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,16 @@ import App from './components/app';
 import reducers from './reducers';
 
 import {Router,Route,indexRoute,browserHistory} from 'react-router';
+import Signin from './components/auth/signin';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
-		<Route path="/" component={App}></Route>
+		<Route path="/" component={App}>
+			<Route path="signin" component={Signin}/>	
+		</Route>
 	</Router>
   </Provider>
   , document.querySelector('.container'));
