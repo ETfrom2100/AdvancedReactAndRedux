c9c8171d0592cd57aa0fcd250d61b452a3af22f3
Leo Lee <nxl1969@gmail.com>
2017-04-17 23:39:11 -0400

    With reduxThunk,a function is returned instead of an object

---
+++ client/src/actions/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const ROOT_URL = 'http://localhost:3090';
+
+
+export function signinUser({email,password}){
+	
+	//with reduxThunk, we return a function instead of an object
+	//reduxThunk gives us arbitrary access to the dispatch function
+	return function(){
+		//submit email/password to the server
+		axios.post(ROOT_URL+'/signin', {email,password}); //{email:email,password:password}
+	
+		//if request is good
+		//- update state to indicate user is authenticated
+		//- save the JWT token
+		//- redirect user to the '/feature'
+	
+	
+		//if request is bad
+		//show an error to the user
+	}
+	
+	
+}
