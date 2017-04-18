7a9b86438664824d8662f42d4e161283c7eca57f
Leo Lee <nxl1969@gmail.com>
2017-04-18 00:10:11 -0400

    import and use CORS

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const router = require('./router');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 
 //DB setup
 mongoose.connect('mongodb://localhost:auth/auth');
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost:auth/auth');
 //morgan--logging framework
 //bodyParser-- parse incoming requests
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({type:'*/*'}));//any incoming requests will be parsed into json
 router(app);
 
