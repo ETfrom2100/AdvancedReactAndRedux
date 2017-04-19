6661489802f205bb189a9de26cf830f685a2b68e
Leo Lee <nxl1969@gmail.com>
2017-04-18 23:11:02 -0400

    save JWT token in localstorage. Create another action to handle the login erros

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,35 +1,42 @@
 import axios from 'axios';
 import {browserHistory} from 'react-router';
 const ROOT_URL = 'http://localhost:3090';
-import {AUTH_USER} from './types';
+import {AUTH_USER, AUTH_ERROR} from './types';
 
 
 export function signinUser({email,password}){
 	
 	//with reduxThunk, we return a function instead of an object
 	//reduxThunk gives us arbitrary access to the dispatch function
-	return function(){
+	return function(dispatch){
 		//submit email/password to the server
 		//{email:email,password:password}
 		axios.post(`${ROOT_URL}/signin`, {email,password})
 		.then(response=>{
 			//if request is good
 			//- update state to indicate user is authenticated
+			
 			dispatch({type:AUTH_USER});
+			console.log(response);
 			//- save the JWT token
+			localStorage.setItem('token',response.data.token);
 			//- redirect user to the '/feature'
 			browserHistory.push('/feature');
 		})
 		.catch(()=>{
 			//if request is bad
 		//show an error to the user
+			dispatch(authError('Bad login info'));
 		})
-	
-		
-	
-	
-		
+
 	}
 	
 	
+}
+
+export function authError(error){
+	return {
+		type:AUTH_ERROR,
+		payload:error
+	}
 }
