6d8bbfa3f2adbeb9fd2be6e85b89a22368c5dd39
Leo Lee <nxl1969@gmail.com>
2017-04-20 00:34:00 -0400

    Added reduxForm validate function.Defined stateless component to render input and errors

--- client/src/components/auth/signup.js
+++ client/src/components/auth/signup.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react';
-import {reduxForm} from 'redux-form';
+import {Field, reduxForm} from 'redux-form';
 import * as actions from '../../actions';
 
+const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
+		<fieldset className="form-group">
+			<label>{label}</label>
+			<input className="form-control" type={type} placeholder={label} {...input} />
+			{touched && error && <span className="error">{error}</span>}
+		</fieldset>
+	)
 class Signup extends Component{
+	
 	render(){
+		const {handleSubmit} = this.props;
 		return (
-			<div>Sign up</div>
+			<form>
+				
+					
+					<Field className="form-control" component={renderField} type="text" label="Email" name="email"/>
+				
+				
+					<Field className="form-control" component={renderField} type="password" label="Password" name="password"/>
+					
+				
+					<Field className="form-control" component={renderField} type="password" label="Confirm Password" name="passwordConfirm"/>
+				
+				<button action="submit" className="btn btn-primary">Sign up!</button>
+			</form>
 		)
 	}
 }
-
-export default Signup;
+function validate(formProps){
+	const errors = {};
+	//console.log(formProps);
+	if(formProps.password !== formProps.passwordConfirm)
+	{
+		errors.password = "Passwords must match";
+	}
+	return errors;
+}
+export default reduxForm({
+	form:'signup',
+	validate
+})(Signup);
