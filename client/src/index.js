b9a1059fdadf5792171f3ebdee706f45612e5273
Leo Lee <nxl1969@gmail.com>
2017-04-30 22:08:08 -0400

    Added IndexRoute; Verify the user before rendering the doms

--- client/src/index.js
+++ client/src/index.js
@@ -6,20 +6,26 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
-import {Router,Route,indexRoute,browserHistory} from 'react-router';
+import {Router,Route,IndexRoute,browserHistory} from 'react-router';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import Feature from './components/feature';
 import reduxThunk from 'redux-thunk';
+import Welcome from './components/welcome';
 import RequireAuth from './components/auth/require_auth';
-
+import {AUTH_USER} from './actions/types';
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+if(token){
+	store.dispatch({type:AUTH_USER});
+}
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
 		<Route path="/" component={App}>
+			<IndexRoute component={Welcome} />
 			<Route path="signin" component={Signin}/>	
 			<Route path="signout" component={Signout}/>	
 			<Route path="signup" component={Signup}/>
